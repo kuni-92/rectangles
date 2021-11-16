f2c05a8c529a15218c2153d3ec83b55976c8ca16
kunikuni03 <49485046+kunikuni03@users.noreply.github.com>
2021-11-16 21:53:18 +0900

    add Rectangle method

--- src/main.rs
+++ src/main.rs
@@ -8,6 +8,14 @@ impl Rectangle {
     fn area(&self) -> u32 {
         self.width * self.height
     }
+
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        self.width > other.width && self.height > other.height
+    }
+
+    fn square(size: u32) -> Rectangle {
+        Rectangle { width: size, height: size }
+    }
 }
 
 fn main() {
@@ -15,4 +23,12 @@ fn main() {
    println!("rect1 is {:?}", rect1);
 
    println!("The area of the rectangle is {} square pixels.", rect1.area());
+
+   let rect2 = Rectangle { width: 10, height: 40 };
+   let rect3 = Rectangle { width: 100, height: 100 };
+
+   println!("can hold test, rect2: {}, rect3: {}", rect1.can_hold(&rect2), rect1.can_hold(&rect3));
+
+   let square = Rectangle::square(30);
+   println!("square test, square: {:?}", square);
 }
